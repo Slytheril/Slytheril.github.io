2afce47ff7d951f8e9bf950f33d443e9383bbff1
NhatTran <nhat.trananh@vn.bosch.com>
2026-03-08 13:17:25 +0000

    update output

--- auto.sh
+++ auto.sh
@@ -6,10 +6,10 @@ echo "Starting CV generation..."
 
 # 1. Convert LaTeX to HTML
 echo "Converting .tex to HTML..."
-pandoc Nhat_CV.tex -o docs/Nhat_CV.html --css style.css --self-contained
+pandoc Nhat_CV.tex -o docs/index.html --css style.css --self-contained
 
 # 2. Convert HTML to PDF
 echo "Generating PDF..."
-wkhtmltopdf docs/Nhat_CV.html output.pdf
+wkhtmltopdf docs/index.html Nhat_CV.pdf
 
 echo "Success! Nhat_CV.pdf has been created."
